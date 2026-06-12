7adff2a603038752474e003b33cf266bf5941966
adina <adina.eni@gmail.com>
2026-06-12 15:13:03 +0300

    modules split

---
+++ pet-clinic-data/src/main/java/com/nightshift/pet_clinic/model/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.nightshift.pet_clinic.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+/**
+ * Created by jt on 7/18/18.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public boolean isNew() {
+        return this.id == null;
+    }
+}
