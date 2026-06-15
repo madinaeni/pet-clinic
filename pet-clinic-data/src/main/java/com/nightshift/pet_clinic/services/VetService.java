e6593fa8c5aeca3bb928ebd123ca5e9be0503d59
adina <adina.eni@gmail.com>
2026-06-15 11:32:05 +0300

    add services

---
+++ pet-clinic-data/src/main/java/com/nightshift/pet_clinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.nightshift.pet_clinic.services;
+
+import com.nightshift.pet_clinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
